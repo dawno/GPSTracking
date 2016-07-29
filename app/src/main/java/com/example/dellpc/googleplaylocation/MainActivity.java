7b16f79f858fda8b0dcf931c95260debd8ef44cb
dawno <utkarshsinghcs@gmail.com>
2016-07-30 01:26:59 +0530

    GPS tracking java class made

--- app/src/main/java/com/example/dellpc/googleplaylocation/MainActivity.java
+++ app/src/main/java/com/example/dellpc/googleplaylocation/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.dellpc.googleplaylocation;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // UI elements
+    private TextView lblLocation;
+    private Button btnShowLocation, btnStartLocationUpdates;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
